1cc8a94dc033de8889cd192d20d6819ca5d8c352
luke73volt <luca.guelfi@outlook.it>
2025-06-01 23:37:35 +0200

    Add next.config.js 1748813854678

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:5000/api/:path*',
+      },
+    ];
+  },
+  experimental: {
+    serverActions: {
+      enabled: true,
+    },
+  },
+};
+
+module.exports = nextConfig;

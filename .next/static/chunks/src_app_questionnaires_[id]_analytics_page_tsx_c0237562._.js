f148e57f3ca2d3368d957b5d89e9b3be81a638f3
luke73volt <luca.guelfi@outlook.it>
2025-06-01 23:38:15 +0200

    Add .next/static/chunks/src_app_questionnaires_[id]_analytics_page_tsx_c0237562._.js 1748813894549

---
+++ .next/static/chunks/src_app_questionnaires_[id]_analytics_page_tsx_c0237562._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2c0aa0da._.js",
+  "static/chunks/src_291f8233._.js"
+],
+    source: "dynamic"
+});
